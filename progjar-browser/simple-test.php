7c5f35c83c4792c80400079d907cbff6ce98cd11
aryawidiap <aryawidiaputra2002@gmail.com>
2023-06-01 11:54:33 +0700

    page for testing put these file into C:\xampp\htdocs and then run xampp Apache and mySQL

---
+++ progjar-browser/simple-test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+bodo
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>test</p>
+    <a href="link-1.php">link 1</a>
+    <a href="link-2.php">link 2</a>
+    <a href="link-3.php">link 3</a>
+</body>
+
+</html>
